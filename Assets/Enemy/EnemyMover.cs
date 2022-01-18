bd3b1509e80bc4705e807c3dd5a1cf106edf0a9d
Bob Griggs <bob.griggs@outlook.com>
2022-01-18 11:15:09 -0600

    127 Added currency system and UI to display Gold.

--- Assets/Enemy/EnemyMover.cs
+++ Assets/Enemy/EnemyMover.cs
@@ -7,12 +7,20 @@ public class EnemyMover : MonoBehaviour
     [SerializeField] List<Waypoint> path = new List<Waypoint>();
     [SerializeField] [Range (0f, 5f)]float speed = 1f;
     
+    Enemy enemy;
+    
     void OnEnable()
     {
        FindPath();
        ReturnToStart();
        StartCoroutine(FollowPath());    
     }
+
+    void Start() 
+    {
+        enemy = GetComponent<Enemy>();
+    }
+
 
     void FindPath()
     {
@@ -50,6 +58,7 @@ public class EnemyMover : MonoBehaviour
             }
         }
 
+        enemy.StealGold();
         gameObject.SetActive(false);
     }
 }
